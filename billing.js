be7ee5d789279307ba5144acdb82b4d7863050fb
Nic <nic.pete@gmail.com>
2018-10-27 06:53:21 -0700

    Adding a billing API

--- billing.js
+++ billing.js
@@ -3,22 +3,22 @@ import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
-    const { storage, source } = JSON.parse(event.body);
-    const amount = calculateCost(storage);
-    const description = "Scratch charge";
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
 
-    // Load our secret key from the environment variables
-    const stripe = stripePackage(process.env.stripeSecretKey);
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
 
-    try {
-        await stripe.charges.create({
-            source,
-            amount,
-            description,
-            currency: "usd"
-        });
-        callback(null, success({ status: true }));
-    } catch (e) {
-        callback(null, failure({ message: e.message }));
-    }
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: "usd"
+    });
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ message: e.message }));
+  }
 }
